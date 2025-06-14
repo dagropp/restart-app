3503bb003903669d486b1815d028d2b50425f24a
Daniel Gropp <dgropp@gmail.com>
2025-06-14 17:58:03 +0300

    added temp fix to test

--- src/@services/api/utils.ts
+++ src/@services/api/utils.ts
@@ -1,4 +1,4 @@
-const API_ROOT = `${import.meta.env.VITE_SERVER_ROOT}/api`;
+const API_ROOT = 'https://restart-server.onrender.com/api';
 
 export const getUrl = (path: string, ...ids: (string | number | undefined)[]) =>
   [API_ROOT, path, ...ids].filter(Boolean).join('/');

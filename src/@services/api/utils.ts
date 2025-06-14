cc1e1cd37000e55b186bbeb9000d3eeb89872016
Daniel Gropp <dgropp@gmail.com>
2025-06-14 17:50:52 +0300

    env changes

--- src/@services/api/utils.ts
+++ src/@services/api/utils.ts
@@ -1,4 +1,4 @@
-const API_ROOT = 'http://localhost:3290/api';
+const API_ROOT = `${import.meta.env.VITE_SERVER_ROOT}/api`;
 
 export const getUrl = (path: string, ...ids: (string | number | undefined)[]) =>
   [API_ROOT, path, ...ids].filter(Boolean).join('/');
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@shared': '/src/@shared',
-      '@context': '/src/@context',
-      '@common': '/src/@common',
-      '@hooks': '/src/@hooks',
-      '@services': '/src/@services',
-      '@utils': '/src/@utils',
-      '@views': '/src/@views',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_SERVER_ROOT,
+    resolve: {
+      alias: {
+        '@shared': '/src/@shared',
+        '@context': '/src/@context',
+        '@common': '/src/@common',
+        '@hooks': '/src/@hooks',
+        '@services': '/src/@services',
+        '@utils': '/src/@utils',
+        '@views': '/src/@views',
+      },
     },
-  },
+  };
 });

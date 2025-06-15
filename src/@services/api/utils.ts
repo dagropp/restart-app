027aa7bb68abf0471d0a7d52944ea7e8ba987f3c
Daniel Gropp <dgropp@gmail.com>
2025-06-15 11:37:42 +0300

    fixed env

--- src/@services/api/utils.ts
+++ src/@services/api/utils.ts
@@ -1,4 +1,4 @@
-const API_ROOT = 'https://restart-server.onrender.com/api';
+const API_ROOT = `${import.meta.env.VITE_SERVER_ROOT}/api`;
 
 export const getUrl = (path: string, ...ids: (string | number | undefined)[]) =>
   [API_ROOT, path, ...ids].filter(Boolean).join('/');

2da24b4f319a5b099a7486d085432fb97311d5fe
danielgropp <dgropp@paloaltonetworks.com>
2025-06-17 17:49:09 +0300

    added mobile tooltip support

--- src/@common/Tooltip/Tooltip.tsx
+++ src/@common/Tooltip/Tooltip.tsx
@@ -1,9 +1,53 @@
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 import MuiTooltip from '@mui/material/Tooltip';
+import { style } from '@utils/style.utils';
 import clsx from 'clsx';
+import { cloneElement, ReactElement, useEffect, useState } from 'react';
 
 import Typography from '../Typography';
 import styles from './styles.module.css';
 import { type TooltipProps } from './types';
+
+type BaseChild = ReactElement<{ onClick?: () => void }>;
+
+const ClickTooltip = ({
+  children,
+  title,
+  open: _open,
+  ...props
+}: TooltipProps) => {
+  const [open, setOpen] = useState(
+    _open ?? (style.isHoverSupported ? undefined : false),
+  );
+
+  const childProps: object = children.props ?? {};
+
+  const onClose = () => setOpen(false);
+  const toggle = () => setOpen((prev) => !prev);
+
+  const clonedChildren =
+    style.isHoverSupported || 'onClick' in childProps
+      ? children
+      : cloneElement(children as BaseChild, { onClick: toggle, ...childProps });
+
+  useEffect(() => {
+    setOpen(_open);
+  }, [_open]);
+
+  return (
+    <ClickAwayListener onClickAway={onClose}>
+      <MuiTooltip title={title} open={open} {...props}>
+        {clonedChildren}
+      </MuiTooltip>
+    </ClickAwayListener>
+  );
+};
+
+const HoverTooltip = ({ children, title, ...props }: TooltipProps) => (
+  <MuiTooltip title={title} {...props}>
+    {children}
+  </MuiTooltip>
+);
 
 const Tooltip = ({
   title = '',
@@ -22,18 +66,21 @@ const Tooltip = ({
     </Typography>
   );
 
-  return (
-    <MuiTooltip
-      title={title ? popper : ''}
-      arrow={arrow}
-      classes={{
-        ...classes,
-        popper: clsx(isComponent && styles.hasComponent, classes?.popper),
-      }}
-      {...props}
-    >
-      {children}
-    </MuiTooltip>
+  const sharedProps = {
+    title: title ? popper : '',
+    arrow,
+    classes: {
+      ...classes,
+      popper: clsx(isComponent && styles.hasComponent, classes?.popper),
+    },
+    children,
+    ...props,
+  };
+
+  return style.isHoverSupported ? (
+    <HoverTooltip {...sharedProps} />
+  ) : (
+    <ClickTooltip {...sharedProps} />
   );
 };
 

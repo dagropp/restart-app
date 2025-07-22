cfd945a7d7c4a18c01e363602fafe062e8373634
danielgropp <dgropp@paloaltonetworks.com>
2025-07-22 14:37:25 +0300

    added cache clearing

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,20 @@ dateService.init();
 
 const queryClient = new QueryClient();
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+
+  caches.keys().then((cacheNames) => {
+    for (const name of cacheNames) {
+      caches.delete(name);
+    }
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <TranslationsContextWrapper>
